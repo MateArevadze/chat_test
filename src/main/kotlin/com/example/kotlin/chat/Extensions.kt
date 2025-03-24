75df1a458e7ab1e21f596f817d4da36f60793d47
Mate <arevadzemate@gmail.com>
2025-03-24 18:19:05 +0400

    xd

--- src/main/kotlin/com/example/kotlin/chat/Extensions.kt
+++ src/main/kotlin/com/example/kotlin/chat/Extensions.kt
@@ -6,9 +6,6 @@ import com.example.kotlin.chat.model.MessageVM
 import com.example.kotlin.chat.model.UserVM
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
-import org.intellij.markdown.flavours.commonmark.CommonMarkFlavourDescriptor
-import org.intellij.markdown.html.HtmlGenerator
-import org.intellij.markdown.parser.MarkdownParser
 import java.net.URL
 
 fun MessageVM.asDomainObject(contentType: ContentType = ContentType.MARKDOWN): Message = Message(
@@ -21,22 +18,12 @@ fun MessageVM.asDomainObject(contentType: ContentType = ContentType.MARKDOWN): M
 )
 
 fun Message.asViewModel(): MessageVM = MessageVM(
-    contentType.render(content),
+    contentType.toString(),
     UserVM(username, URL(userAvatarImageLink)),
     sent,
     id
 )
-fun MessageVM.asRendered(contentType: ContentType = ContentType.MARKDOWN): MessageVM =
-    this.copy(content = contentType.render(this.content))
 
 fun Flow<Message>.mapToViewModel(): Flow<MessageVM> = map { it.asViewModel() }
 
 fun List<Message>.mapToViewModel(): List<MessageVM> = map { it.asViewModel() }
-
-fun ContentType.render(content: String): String = when (this) {
-    ContentType.PLAIN -> content
-    ContentType.MARKDOWN -> {
-        val flavour = CommonMarkFlavourDescriptor()
-        HtmlGenerator(content, MarkdownParser(flavour).buildMarkdownTreeFromString(content), flavour).generateHtml()
-    }
-}
--- src/main/kotlin/com/example/kotlin/chat/service/PersistentMessageService.kt
+++ src/main/kotlin/com/example/kotlin/chat/service/PersistentMessageService.kt
@@ -1,34 +1,42 @@
 package com.example.kotlin.chat.service
 
 import com.example.kotlin.chat.asDomainObject
-import com.example.kotlin.chat.asRendered
 import com.example.kotlin.chat.mapToViewModel
-import com.example.kotlin.chat.model.MessageList
+import com.example.kotlin.chat.model.Message
 import com.example.kotlin.chat.model.MessageVM
 import kotlinx.coroutines.flow.*
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
-class PersistentMessageService(private val msgList : MessageList) : MessageService {
+class PersistentMessageService : MessageService {
+    private val messages = LinkedList<Message>();
+    private val maxSize = 500;
 
     val sender: MutableSharedFlow<MessageVM> = MutableSharedFlow()
 
     override  suspend fun latest(): Flow<MessageVM>  {
-        return msgList.getMessages().mapToViewModel().asFlow()
+        return messages.mapToViewModel().asFlow()
     }
 
     override suspend fun after(messageId: String): Flow<MessageVM> {
-        return msgList.getMessages().mapToViewModel().asFlow()
+        return messages.filter{msg -> msg.id == messageId}
+                        .mapToViewModel().asFlow()
     }
-
 
     override suspend fun stream(): Flow<MessageVM> = sender
 
+    @Synchronized
     override suspend fun post(messages: Flow<MessageVM>) {
-
+        messages.collect() {
+            if (this.messages.size == maxSize) {
+                this.messages.removeFirst()
+            }
+            this.messages.add(it.asDomainObject())
+        }
     }
 
-
     override suspend fun clean() {
+        messages.clear()
     }
 }
