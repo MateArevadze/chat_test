93ed885b2baf998744d53de5d92b821941911842
Mate <arevadzemate@gmail.com>
2025-03-26 17:41:09 +0400

    added id parameter to messageVM

--- src/main/kotlin/com/example/kotlin/chat/model/MessageVM.kt
+++ src/main/kotlin/com/example/kotlin/chat/model/MessageVM.kt
@@ -5,5 +5,5 @@ import java.time.Instant
 data class MessageVM(val content: String,
                      val gameId: String,
                      val userId: String,
-                     val sent: Instant)
-
+                     val sent: Instant,
+                     var id: String? = null)
