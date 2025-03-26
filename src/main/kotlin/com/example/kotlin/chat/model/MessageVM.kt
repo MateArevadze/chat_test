13282466fa241e50af80e7c3a9cc8a928c902b97
MariamTsik <mariam.tsikarishvili@provablyfair.studio>
2025-03-26 15:28:57 +0400

    change messageVM

--- src/main/kotlin/com/example/kotlin/chat/model/MessageVM.kt
+++ src/main/kotlin/com/example/kotlin/chat/model/MessageVM.kt
@@ -3,6 +3,7 @@ package com.example.kotlin.chat.model
 import java.time.Instant
 
 data class MessageVM(val content: String,
+                     val gameId: String,
                      val user: UserVM,
                      val sent: Instant,
                      val id: String? = null)
