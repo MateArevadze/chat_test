0144d0665fa0e94d7a01010149e4840f9c55918e
Mate <arevadzemate@gmail.com>
2025-03-26 12:40:23 +0400

    zzz

--- src/main/kotlin/com/example/kotlin/chat/repository/MessageRepository.kt
+++ src/main/kotlin/com/example/kotlin/chat/repository/MessageRepository.kt
@@ -12,19 +12,9 @@ interface MessageRepository : CoroutineCrudRepository<Message, String> {
     @Query("""
         SELECT * FROM (
             SELECT * FROM MESSAGES
-            ORDER BY "SENT" DESC
+            ORDER BY sent DESC
             LIMIT 10
-        ) ORDER BY "SENT"
+        ) ORDER BY sent
     """)
     fun findLatest(): Flow<Message>
-
-    // language=SQL
-    @Query("""
-        SELECT * FROM (
-            SELECT * FROM MESSAGES
-            WHERE SENT > (SELECT SENT FROM MESSAGES WHERE ID = :id)
-            ORDER BY "SENT" DESC 
-        ) ORDER BY "SENT"
-    """)
-    fun findLatest(@Param("id") id: String): Flow<Message>
 }
